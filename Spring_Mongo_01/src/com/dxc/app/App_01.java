15589e00992f9d704c05ec7ef4f6c626c3847912
phemasri <phemasri@host.docker.internal>
2020-09-27 11:21:53 +0530

    commit

---
+++ Spring_Mongo_01/src/com/dxc/app/App_01.java
@@ -0,0 +1,10 @@
+package com.dxc.app;
+
+public class App_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Spring_Mongo_01/src/com/dxc/app/ApplicationConfiguration.java
@@ -0,0 +1,10 @@
+package com.dxc.app;
+
+public class ApplicationConfiguration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
